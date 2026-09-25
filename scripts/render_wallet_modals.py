8857a9e58bec5d7c5b720b19e1f0c0cbcb7795f9
Betz Lenteria <wilbertlenteria3@gmail.com>
2026-09-25 14:22:38 +0800

    Update render_wallet_modals.py

--- scripts/render_wallet_modals.py
+++ scripts/render_wallet_modals.py
@@ -17,7 +17,8 @@ def _stub_url_for(endpoint, **values):
 
 
 MODALS = ["sendModal", "gcashModal", "settingsModal", "referralModal",
-          "dailyTaskModal", "streamModal", "receiveModal", "claimModal", "fvIntroModal"]
+          "dailyTaskModal", "streamModal", "receiveModal", "claimModal", "fvIntroModal",
+          "fvDeVerifyModal"]
 
 env = Environment(loader=FileSystemLoader(TEMPLATES), autoescape=True)
 env.globals["url_for"] = _stub_url_for
